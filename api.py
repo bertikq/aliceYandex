adb523184e1d744821e97894afa55e2e38c5a6d2
Aleksey <permenev.alex@ya.ru>
2019-11-30 15:25:25 +0400

    upd

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ import random
 from dialogs import data
 import json
 from flask import Flask, request
+import textTypeAnswer
 
 app = Flask(__name__)
 sessionStorage = {}
@@ -10,7 +11,6 @@ sessionStorage = {}
 
 @app.route("/", methods=['POST'])
 def main():
-    print("HELLO!")
     response = {
         "version": request.json['version'],
         "session": request.json['session'],
@@ -34,9 +34,10 @@ def handle_dialog(req, res):
                 "Ну начнем",
                 "Погнали",
             ],
-            'cur_theme': 'общая',
+            'cur_theme': 'default',
             'cur_quest': 0,
             'cur_dif': 0,
+            'cur_lose': 0,
             'done': {},
             'count_win_quests': 0,
             'count_lose_quests': 0,
@@ -47,6 +48,7 @@ def handle_dialog(req, res):
             for startButton in sessionStorage[user_id]['suggests']
         ]
         return
+    print(req['request']['original_utterance'].lower())
     if req['request']['original_utterance'].lower() in {
         "ну начнем",
         "погнали",
@@ -59,45 +61,69 @@ def handle_dialog(req, res):
 
 def check_answer(user_id, req, res):
     session = sessionStorage[user_id]
-    if not (req['request']['original_utterance'].lower() in
-            data['themes'][session['cur_theme']]['questions'][session['cur_quest']]['answers']):
+    if (data['themes'][session['cur_theme']]['questions'][session['cur_dif']][
+        session['cur_quest']]['type'] == 'text' and textTypeAnswer.equals(req['request']['original_utterance'],
+                                                                          data['themes'][session['cur_theme']][
+                                                                              'questions'][
+                                                                              session['cur_dif']][
+                                                                              session['cur_quest']]['answers']) > 0.5):
+        win_answer(user_id, res)
+    elif not (req['request']['original_utterance'].lower() in
+              data['themes'][session['cur_theme']]['questions'][session['cur_dif']][session['cur_quest']]['answers']):
         res['response']['text'] = data['samples']['quest_repeat'].format("\n".join([
             "{}. {}".format(i + 1, item)
-            for i, item in enumerate(data['themes'][session['cur_theme']]['questions'][session['cur_quest']]['answers'])
+            for i, item in enumerate(
+                data['themes'][session['cur_theme']]['questions'][session['cur_dif']][session['cur_quest']]['answers'])
         ]))
         res['response']['buttons'] = get_suggests(user_id)
-        return
-    if req['request']['original_utterance'].lower() == data['themes'][session['cur_theme']]['questions'][session['cur_quest']]['answers'][0]:
+    elif req['request']['original_utterance'].lower() == \
+            data['themes'][session['cur_theme']]['questions'][session['cur_dif']][session['cur_quest']]['answers'][
+                0].lower():
         win_answer(user_id, res)
-        return
-    lose_answer(user_id, res)
+    else:
+        lose_answer(user_id, res)
 
 
 def win_answer(user_id, res):
     session = sessionStorage[user_id]
-    session['done']['общая']['count_quest'].add(session['cur_quest'])
+    session['done'][session['cur_theme']]['count_quest'].add(session['cur_quest'])
     session['cur_dif'] += 1
     session['count_win_quests'] += 1
+    if session['cur_dif'] > 2:
+        if not switch_theme(user_id):
+            send_results(user_id, res)
+            sessionStorage[user_id] = session
+            return
+        else:
+            session['cur_dif'] = 0
     sessionStorage[user_id] = session
-    next_question(user_id)
+    next_question(user_id, res)
     write_response(user_id, res, True)
 
 
 def lose_answer(user_id, res):
     session = sessionStorage[user_id]
-    session['done']['общая']['count_quest'].add(session['cur_quest'])
+    session['done'][session['cur_theme']]['count_quest'].add(session['cur_quest'])
     session['cur_dif'] = 0
+    session['cur_lose'] += 1
     session['count_lose_quests'] += 1
+    if session['cur_lose'] > 2:
+        if not switch_theme(user_id):
+            send_results(user_id, res)
+            sessionStorage[user_id] = session
+            return
+        else:
+            session['cur_lose'] = 0
     sessionStorage[user_id] = session
-    next_question(user_id)
+    next_question(user_id, res)
     write_response(user_id, res, False)
 
 
 def start_dialog(user_id, res):
     session = sessionStorage[user_id]
-    session['done']['общая'] = {'count_quest': set()}
+    session['done'][session['cur_theme']] = {'count_quest': set()}
     sessionStorage[user_id] = session
-    next_question(user_id)
+    next_question(user_id, res)
     res['response']['text'] = '''
         Первый вопрос.
         {}
@@ -105,36 +131,51 @@ def start_dialog(user_id, res):
         {}
     '''.format(
         data['themes'][session['cur_theme']]['questions'][session['cur_dif']][session['cur_quest']]['body'],
-        "\n".join(["{}. {}".format(i + 1, item) for i, item in enumerate(data['themes'][session['cur_theme']]['questions'][session['cur_dif']][session['cur_quest']]['answers'])])
+        "\n".join(["{}. {}".format(i + 1, item) for i, item in enumerate(
+            data['themes'][session['cur_theme']]['questions'][session['cur_dif']][session['cur_quest']]['answers'])])
     )
-    res['response']['buttons'] = get_suggests(user_id)
+    if data['themes'][session['cur_theme']]['questions'][session['cur_dif']][session['cur_quest']]['type'] != 'text':
+        res['response']['buttons'] = get_suggests(user_id)
 
 
 def write_response(user_id, res, isWin):
     session = sessionStorage[user_id]
-    res['response']['text'] = \
-        data['samples']['win_ans'].format(
-            session['cur_theme'],
-            data['themes'][session['cur_theme']]['questions'][session['cur_dif']][session['cur_quest']]['body'],
-            "\n".join(["{}. {}".format(i + 1, item) for i, item in enumerate(data['themes'][session['cur_theme']]['questions'][session['cur_dif']][session['cur_quest']]['answers'])])
-        ) if isWin else \
-        data['samples']['lose_ans'].format(
-            data['themes'][session['cur_theme']]['questions'][session['cur_dif']][session['cur_quest']]['resource'],
-            session['cur_theme'],
-            data['themes'][session['cur_theme']]['questions'][session['cur_dif']][session['cur_quest']]['body'],
-            "\n".join(["{}. {}".format(i + 1, item) for i, item in enumerate(data['themes'][session['cur_theme']]['questions'][session['cur_dif']][session['cur_quest']]['answers'])])
-        )
-    res['response']['buttons'] = get_suggests(user_id)
+    if data['themes'][session['cur_theme']]['questions'][session['cur_dif']][session['cur_quest']]['type'] != 'text':
+        res['response']['text'] = \
+            data['samples']['win_ans'].format(
+                session['cur_theme'],
+                data['themes'][session['cur_theme']]['questions'][session['cur_dif']][session['cur_quest']]['body'],
+                "\n".join(["{}. {}".format(i + 1, item) for i, item in enumerate(
+                    data['themes'][session['cur_theme']]['questions'][session['cur_dif']][session['cur_quest']][
+                        'answers'])])
+            ) if isWin else data['samples']['lose_ans'].format(
+                data['themes'][session['cur_theme']]['questions'][session['cur_dif']][session['cur_quest']]['resource'],
+                data['themes'][session['cur_theme']]['questions'][session['cur_dif']][session['cur_quest']]['body'],
+                "\n".join(["{}. {}".format(i + 1, item) for i, item in enumerate(
+                    data['themes'][session['cur_theme']]['questions'][session['cur_dif']][session['cur_quest']][
+                        'answers'])])
+            )
+        res['response']['buttons'] = get_suggests(user_id)
+    else:
+        res['response']['text'] = \
+            data['samples']['win_ans'].format(
+                session['cur_theme'],
+                data['themes'][session['cur_theme']]['questions'][session['cur_dif']][session['cur_quest']]['body']
+            ) if isWin else data['samples']['lose_ans'].format(
+                data['themes'][session['cur_theme']]['questions'][session['cur_dif']][session['cur_quest']]['resource'],
+                data['themes'][session['cur_theme']]['questions'][session['cur_dif']][session['cur_quest']]['body']
+            )
 
 
-def next_question(user_id):
+def next_question(user_id, res):
     session = sessionStorage[user_id]
-    if len(session['done'][session['cur_theme']]['count_quest']) > 3:
+    if len(data['themes'][session['cur_theme']]['questions'][session['cur_dif']]) == \
+            len(session['done'][session['cur_theme']]['count_quest']):
         if not switch_theme(user_id):
-            send_results(user_id)
-        return
+            send_results(user_id, res)
+            return
     while True:
-        num_quest = random.randrange(0, len(data['themes'][session['cur_theme']]['questions'][session['cur_dif']]) - 1)
+        num_quest = random.randrange(0, len(data['themes'][session['cur_theme']]['questions'][session['cur_dif']]))
         if not (num_quest in session['done'][session['cur_theme']]['count_quest']):
             break
     session['cur_quest'] = num_quest
@@ -149,15 +190,14 @@ def switch_theme(user_id):
         session['done'][i] = session['done'].get(i, default={'count_quest': []})
         if len(session['done'][i]['count_quest']) == 0:
             session['cur_theme'] = i
-            session['cur_dif'] = 0
             sessionStorage[user_id] = session
             return True
     sessionStorage[user_id] = session
     return False
 
 
-def send_results(user_id):
-    pass
+def send_results(user_id, res):
+    res['response']['text'] = "Молодец, пока!"
 
 
 def get_suggests(user_id):
--- textTypeAnswer.py
+++ textTypeAnswer.py
@@ -1,10 +1,10 @@
 def equals(answer, info):
     masAnswer = set(answer.split(' '))
     masInfo = set(info.split(' '))
-    infoCountWord = len(masInfo) / 2
+    infoCountWord = len(masAnswer)
     countEqualWords = 0.0
-    for word in masInfo:
-        if masAnswer.__contains__(word):
+    for word in masAnswer:
+        if word in masInfo:
             countEqualWords += 1
     return countEqualWords / infoCountWord
 

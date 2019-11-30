bd39e8c597a46dde3cbdde47932a66c2b5e1ebbe
bertikq <50046409+bertikq@users.noreply.github.com>
2019-11-30 12:24:45 +0400

    change doc

--- dialogs.py
+++ dialogs.py
@@ -4,13 +4,21 @@ data = {
             'questions': [
                 [
                     {
+                        'type': 'simple',
                         'body': 'Ты тут?',
                         'answers': ['да', 'нет'],
                         'resource': 'https://ru.wikipedia.org/wiki/Yes_(%D0%B3%D1%80%D1%83%D0%BF%D0%BF%D0%B0)',
                     },
                     {
+                        'type': 'simple',
                         'body': 'Ты меня слышишь?',
                         'answers': ['да', 'нет', 'наверное'],
+                        'resource': 'https://ru.wikipedia.org/wiki/Yes_(%D0%B3%D1%80%D1%83%D0%BF%D0%BF%D0%B0)',
+                    },
+                    {
+                        'type': 'text',
+                        'body': 'Как ты понимаешь что такое гит?',
+                        'answers': "распределённая система управления версиями. Проект был создан Линусом Торвальдсом для управления разработкой ядра Linux, первая версия выпущена 7 апреля 2005 года. На сегодняшний день его поддерживает Джунио Хамано.",
                         'resource': 'https://ru.wikipedia.org/wiki/Yes_(%D0%B3%D1%80%D1%83%D0%BF%D0%BF%D0%B0)',
                     }
                 ]
